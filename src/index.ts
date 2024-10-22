b9ecd2f7743e0ae7f9933db1a0dfe877a9b14be5
ukyogw <ukyogw@gmail.com>
2024-10-22 22:51:08 +0900

    不要処理の削除

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
 import { Main } from './Main';
 
-//const videoElement = document.createElement('video');
 const videoElement = <HTMLVideoElement>document.getElementById('video');
 console.log({ videoElement });
-const width = '600px';
-const height = '400px';
-
-//setElementDimensions(videoElement, width, height);
-//setElementDimensions(canvasElement, width, height);
-
-// 指定した要素の幅・高さを設定
-function setElementDimensions(
-  element: HTMLElement | null,
-  width: string,
-  height: string
-) {
-  element!.style.width = width;
-  element!.style.height = height;
-}
 
 navigator.mediaDevices
   .getUserMedia({ video: true })
   .then((stream) => {
     videoElement.srcObject = stream;
     videoElement.autoplay = true;
-    //videoElement.play();
+
     videoElement.addEventListener('loadeddata', () => {
       new Main(videoElement); // Main クラスに video を渡す
     });
