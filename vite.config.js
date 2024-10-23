2d56177a51c5352bfab695a3ca6acd2d0259f0a8
ukyogw <ukyogw@gmail.com>
2024-10-23 09:37:05 +0900

    train page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 export default {
   build: {
     rollupOptions: {
+      input: {
+        main: 'index.html', // メインページ
+        train: 'train.html', // トレーニングページ
+      },
       output: {
-        entryFileNames: 'index.js',
-        assetFileNames: '[name][extname]',
+        entryFileNames: '[name].js', // ファイル名をページ名で出力
+        assetFileNames: 'assets/[name][extname]', // アセットの出力先
       },
     },
   },
