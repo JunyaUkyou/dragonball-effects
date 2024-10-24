19521eab45721c632d27e9f124315afa479034bc
ukyogw <ukyogw@gmail.com>
2024-10-24 22:28:34 +0900

    カメラのアスペクト比

--- src/bigbanattack/Main.ts
+++ src/bigbanattack/Main.ts
@@ -27,12 +27,7 @@ export class Main {
     this.scene = new THREE.Scene();
 
     // カメラを追加
-    this.camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
+    this.camera = new THREE.PerspectiveCamera(75, 1300 / 600, 1, 1000);
     this.camera.position.set(0, 0, 300);
 
     // レンダラーを追加
